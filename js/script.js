6396c47d0e3b900c9928bbffa3798c052f3b7241
Niccolò Arcioni <niccoloarcioni@gmail.com>
2021-03-30 17:21:59 +0200

    Inserimento Bonus n.1

--- js/script.js
+++ js/script.js
@@ -2,14 +2,28 @@ var app = new Vue ({
     el: '#root',
     data: {
         albums: [],
+        albumsGender: [],
+        genreChoosen: ''
     },
     created() {
         axios 
             .get('https://flynn.boolean.careers/exercises/api/array/music')
             .then(function (result) {
-                console.log(result);
                 app.albums = result.data.response;
-                console.log(app.albums);
+                let i = 0;
+                while (i < app.albums.length) {
+                    if (!app.albumsGender.includes(app.albums[i].genre)) {
+                        app.albumsGender.push(app.albums[i].genre);
+                    }
+                    i++;
+                }
             })
+    },
+    methods: {
+        setGenreChoosen: function() {
+            let value = document.getElementById('picker').value;
+            console.log(value);
+            this.genreChoosen = value;
+        }
     }
-})
+}) 
